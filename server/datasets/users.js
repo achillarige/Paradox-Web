e6025b899934d43494a4e311556666f68143d2cc
Sahas Dendukuri <sahasd@users.noreply.github.com>
2016-04-15 20:59:17 -0700

    connect to db in module

--- server/datasets/users.js
+++ server/datasets/users.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+mongoose.connect('http://localhost/users') //users will stored in database users and collection User
 module.exports = mongoose.model('User',{
 	email: String,
 	username: String,
